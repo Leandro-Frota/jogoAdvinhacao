3642b4623d3c7a218ad94404befe1603aec63579
Leandro-Frota <108706993+Leandro-Frota@users.noreply.github.com>
2023-12-25 22:25:02 -0300

    Adicionei o botão de reset e fiz refatoração

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 
-
+// varáveis
 const randomNumber = Math.round(Math.random() * 10);
+const btnTry = document.querySelector('#btnTry'); //guarda todo elemento HTML
+const btnReset = document.querySelector('#btnReset'); //pega todo o elemento HTML
+const screen1 = document.querySelector("#screen1");
+const screen2= document.querySelector('#screen2');
+const inputNumber = document.querySelector('#inputNumber'); 
+const result = document.querySelector("#result"); // guarda o elemento HTML
 let xAttempts = 1
 
-console.info(randomNumber)
+function resetClick(){
+    screen1.classList.remove('hide');
+    screen2.classList.add('hide');
+    xAttempts=1
+}
 
-inputButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    const inputNumber = document.querySelector('#inputNumber'); //pega todo o elemento HTML
-    const inputButton = document.querySelector('#inputButton'); //guarda todo elemento HTML
-    const result = document.querySelector("#result"); // guarda o elemento HTML
+btnTry.addEventListener('click', (e) => {
+    e.preventDefault(); // não envia o formulário, característica de um botão dentro de um form
 
     const x = inputNumber.value // guarda na variável o valor do input em formato string
 
     if (Number(x) == randomNumber) { 
-        document.querySelector("#screen1").classList.add("hide"); // procura o elemento de id screen1 e adiciona a classe a palavra hide
-        document.querySelector('#screen2').classList.remove("hide");//procura o elemento de id screen2 e remove a classe a palavra hide
+        screen1.classList.add("hide"); // procura o elemento de id screen1 e adiciona a classe a palavra hide
+        screen2.classList.remove("hide");//procura o elemento de id screen2 e remove a classe a palavra hide
       
         result.innerText = `Acertou em ${xAttempts} tentativa(s)`; // na variavel result, que guarda o elemento h2 de classe result, troca/altera/inclui o texto
     
@@ -23,6 +30,10 @@ inputButton.addEventListener('click', (e) => {
 
     }
 
+    inputNumber.value="";
     xAttempts++
 
 })
+
+//função callbacck
+btnReset.addEventListener('click',resetClick)
